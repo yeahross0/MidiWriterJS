b61fc1d2757c2fad4ba0233b67d31ad1ce08b9d4
Garrett Grimm <grimmdude@gmail.com>
2021-11-21 17:25:48 -0800

    Support delta param in ControllerChangeEvent

--- src/meta-events/controller-change-event.js
+++ src/meta-events/controller-change-event.js
@@ -3,14 +3,19 @@ import {Utils} from '../utils.js';
 
 /**
  * Holds all data for a "controller change" MIDI event
- * @param {object} fields {controllerNumber: integer, controllerValue: integer}
+ * @param {object} fields {controllerNumber: integer, controllerValue: integer, delta: integer}
  * @return {ControllerChangeEvent}
  */
 class ControllerChangeEvent {
 	constructor(fields) {
+		// Set default fields
+		fields = Object.assign({
+			delta: 0x00,
+		}, fields);
+
 		this.type = 'controller';
 		// delta time defaults to 0.
-		this.data = Utils.numberToVariableLength(0x00).concat(Constants.CONTROLLER_CHANGE_STATUS, fields.controllerNumber, fields.controllerValue);
+		this.data = Utils.numberToVariableLength(fields.delta).concat(Constants.CONTROLLER_CHANGE_STATUS, fields.controllerNumber, fields.controllerValue);
 	}
 }
 
